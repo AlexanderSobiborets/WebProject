c0fb2aaeb8d133da2938bc512356ddd0c22ed473
AlexanderSobiborets <alexsobiborets@gmail.com>
2022-05-19 13:41:05 +0300

    optimization site for mobile devices

--- src/js/demo.js
+++ src/js/demo.js
@@ -2,3 +2,12 @@ const demo = () => 'Webpack Boilerplate v5.12.1 - SASS/PostCSS, ES6/7, browser s
 
 // eslint-disable-next-line no-console
 console.log(demo());
+
+const overlay = document.querySelector('div.section-about__video-overlay');
+if (overlay) {
+    overlay.addEventListener('click', () => {
+        const video = document.querySelector('video.section-about__video-player'); 
+        overlay.style.visibility = 'none';
+        video.play();
+    });
+}
